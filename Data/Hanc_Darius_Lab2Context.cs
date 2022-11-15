2d716c6286376b6aea58df26cbfd9a13577d7e72
Darius Hanc <hancdarius@gmail.com>
2022-11-15 19:53:24 +0200

    Add project files.

---
+++ Data/Hanc_Darius_Lab2Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hanc_Darius_Lab2.Models;
+
+namespace Hanc_Darius_Lab2.Data
+{
+    public class Hanc_Darius_Lab2Context : DbContext
+    {
+        public Hanc_Darius_Lab2Context (DbContextOptions<Hanc_Darius_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hanc_Darius_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Hanc_Darius_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Hanc_Darius_Lab2.Models.Author> Author { get; set; }
+
+        public DbSet<Hanc_Darius_Lab2.Models.Category> Category { get; set; }
+
+        public DbSet<Hanc_Darius_Lab2.Models.Member> Member { get; set; }
+
+        public DbSet<Hanc_Darius_Lab2.Models.Borrowing> Borrowing { get; set; }
+    }
+}
---
+++ Pages/Publishers/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Hanc_Darius_Lab2.Data;
+using Hanc_Darius_Lab2.Models;
+
+namespace Hanc_Darius_Lab2.Pages.Publishers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Hanc_Darius_Lab2.Data.Hanc_Darius_Lab2Context _context;
+
+        public CreateModel(Hanc_Darius_Lab2.Data.Hanc_Darius_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+                return Page();
+            }
+
+            _context.Publisher.Add(Publisher);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
